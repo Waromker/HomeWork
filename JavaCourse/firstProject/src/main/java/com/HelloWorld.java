66b018adff88a0eff02c88ea189543b1d45f532c
Waromker <kirx23@gmail.com>
2015-12-14 14:23:17 +0200

    Add new

---
+++ JavaCourse/firstProject/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by air on 14.12.15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
